1bb2d936ff3c22e28f8852d7c5ec1c14f5b8d810
Nehemias Ulloa <nulloa1@iastate.edu>
2016-11-29 21:46:08 -0600

    Modified the data gathering mechanism

--- SimStudy.R
+++ SimStudy.R
@@ -1,21 +1,62 @@
 library(laGP)
-library(ggplot2)
+require(MASS)
+require(plyr)
+require(reshape2)
+require(ggplot2)
+
+# Set a seed for repeatable plots
+set.seed(12345)
+
 
 ## build up a design with N=~40K locations
-x <- seq(-2, 2, by=0.02)
-X <- as.matrix(expand.grid(x, x))
-Z <- f2d(X)
-## local analysis, first pass
-Xref <- matrix(c(-1.725, 1.725), nrow=TRUE)
+x.star <- seq(-2, 2, length=25)
+X <- as.matrix(expand.grid(x.star, x.star))
 
-theta <- seq(-2, 2, by=0.2)
+calcSigma <- function(X1,X2,l=1){
+  Sigma <- matrix(rep(0, length(X1)*length(X2)), nrow=length(X1))
+  for (i in 1:nrow(Sigma)) {
+    for (j in 1:ncol(Sigma)) {
+      Sigma[i,j] <- exp(-0.5*(abs(X1[i]-X2[j])/l)^2)
+    }
+  }
+  return(Sigma)
+}
+
+## creates the response
+getresp <- function(x,theta){
+  sigma <- calcSigma(x,x)
+  values <- matrix(rep(0,length(x)), ncol=1)
+  values <- mvrnorm(1, rep(theta, length(x)), sigma)
+  values <- data.frame(x=x, y=values)
+}
+
+## local analysis, first pass
+Xref <- matrix(c(-1.5, 1.83), nrow=TRUE)
+
+
+## Initialize
+iter        <- 100
+theta       <- seq(-1, 1, length=iter)
+truepred    <- rep(NA, iter)
+nnThetaEst  <- rep(NA, iter)
+nnPred      <- rep(NA, iter)
+alcThetaEst <- rep(NA, iter)
+alcPred     <- rep(NA, iter)
+
 for(i in 1:length(theta)){
-  nnThetaEst[i]  <- laGP(Xref, 6, 500, X, Z, d=theta[i], method="nn")$mle$d
-  nnPred[i]      <- laGP(Xref, 6, 500, X, Z, d=theta[i], method="nn")$mean
-  alcThetaEst[i] <- laGP(Xref, 6, 500, X, Z, d=theta[i], method="alc", alc.gpu=TRUE)$mle$d
-  alcPred[i]     <- laGP(Xref, 6, 500, X, Z, d=theta[i], method="alc", alc.gpu=TRUE)$mean
+  Z1 <- getresp(X[,1], theta[i])$y
+  Z2 <- getresp(X[,2], theta[i])$y
+  Z0 <- Z1*Z2
+  
+  truepred[i]    <- Z0[579]
+  nngp           <- laGP(Xref, 6, 100, X, Z0, method="nn")
+  nnThetaEst[i]  <- nngp$mle$d
+  nnPred[i]      <- nngp$mean
+  algp           <- laGP(Xref, 6, 100, X, Z0, method="alc", alc.gpu=TRUE)
+  alcThetaEst[i] <- algp$mle$d
+  alcPred[i]     <- algp$mean
 }
 
 
-SimRes <- cbind(nnThetaEst, nnPred, alcThetaEst, alcPred)
+SimRes <- cbind(theta,truepred, nnThetaEst, nnPred, alcThetaEst, alcPred)
 save(SimRes, file="SimRes.RData")
