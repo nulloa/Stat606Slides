b6a06370e16322807d48a3213feacdd820737d96
Nehemias Ulloa <nulloa1@iastate.edu>
2016-11-30 14:10:20 -0600

    Corrected simulation for theta

--- SimStudy.R
+++ SimStudy.R
@@ -10,34 +10,36 @@ set.seed(12345)
 
 ## build up a design with N=~40K locations
 n <- 10
-x.star <- seq(-n, n, length=50)
+x.star <- seq(-n, n, by=1)
 X <- as.matrix(expand.grid(x.star, x.star))
+#plot(X[,1],X[,2])
 
-calcSigma <- function(X1,X2,l=1){
-  Sigma <- matrix(rep(0, length(X1)*length(X2)), nrow=length(X1))
+calcSigma <- function(X,theta=1){
+  Sigma <- matrix(rep(0, nrow(X)*nrow(X)), nrow=nrow(X))
   for (i in 1:nrow(Sigma)) {
     for (j in 1:ncol(Sigma)) {
-      Sigma[i,j] <- exp(-0.5*(abs(X1[i]-X2[j])/l)^2)
+      Sigma[i,j] <- exp(-(sqrt((X[i,1]-X[j,1])^2+(X[i,2]-X[j,2])^2)^2/theta))
     }
   }
   return(Sigma)
 }
 
+
 ## creates the response
 getresp <- function(x,theta){
-  sigma <- calcSigma(x,x)
-  values <- matrix(rep(0,length(x)), ncol=1)
-  values <- mvrnorm(1, rep(theta, length(x)), sigma)
-  values <- data.frame(x=x, y=values)
+  sigma <- calcSigma(x,theta)
+  values <- matrix(rep(0,nrow(x)), ncol=1)
+  values <- mvrnorm(1, rep(0, nrow(x)), sigma)
+  values <- data.frame(y=values)
 }
 
 ## local analysis, first pass
-Xref <- matrix(c(-1.5, 1.83), nrow=TRUE)
+Xref <- matrix(c(-2, 2), nrow=TRUE)
 
 
 ## Initialize
 iter        <- 100
-theta       <- seq(-1, 1, length=iter)
+theta       <- seq(0.01, 10, length=iter)
 truepred    <- rep(NA, iter)
 nnThetaEst  <- rep(NA, iter)
 nnPred      <- rep(NA, iter)
@@ -45,11 +47,9 @@ alcThetaEst <- rep(NA, iter)
 alcPred     <- rep(NA, iter)
 
 for(i in 1:length(theta)){
-  Z1 <- getresp(X[,1], theta[i])$y
-  Z2 <- getresp(X[,2], theta[i])$y
-  Z0 <- Z1*Z2
+  Z0 <- getresp(X, theta[i])$y
   
-  truepred[i]    <- Z0[579]
+  truepred[i]    <- Z0[261]
   nngp           <- laGP(Xref, 6, 100, X, Z0, method="nn")
   nnThetaEst[i]  <- nngp$mle$d
   nnPred[i]      <- nngp$mean
